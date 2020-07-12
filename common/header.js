ed55a7b6afbddbdb73d03680c07794d094072b59
SamChu <sam830917@gmail.com>
2020-07-12 18:24:31 -0500

    First commit

---
+++ common/header.js
@@ -0,0 +1,24 @@
+header = '<header id="header">\
+<div class="d-flex flex-column">\
+  <div class="profile">\
+	<img src="assets/images/profile-img.png" alt="" class="img-fluid rounded-circle">\
+	<h1 class="text-light"><a href="index.html">Chien-Yu Chu</a></h1>\
+	<div class="social-links mt-3 text-center">\
+	  <a href="mailto:sam830917@gmail.com" class="google-plus"><i class="bx bx-envelope"></i></a>\
+	  <a href="https://www.linkedin.com/in/%E5%BB%BA%E5%AE%87-%E6%9C%B1-732147191/" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>\
+	  <a href="https://github.com/sam830917" target="_blank"><i class="bx bxl-github"></i></a>\
+	</div>\
+  </div>\
+  <nav class="nav-menu">\
+	<ul>\
+	  <li id="homeLink"><a href="index.html"><i class="bx bx-home"></i> <span>Home</span></a></li>\
+	  <li id="aboutLink"><a href="AboutPage.html"><i class="bx bx-user"></i> <span>About</span></a></li>\
+	  <li id="projectLink"><a href="ProjectPage.html"><i class="bx bx-book-content"></i> Projects</a></li>\
+	  <li id="resumeLink"><a href="ResumePage.html"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>\
+	</ul>\
+  </nav><!-- .nav-menu -->\
+  <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>\
+</div>\
+</header>\
+';
+document.write(header);
